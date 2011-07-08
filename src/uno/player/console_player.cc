3c5bed817c0e05f4b7fd5893685eade747f6e31c
erenon <erenon2@gmail.com>
2011-07-08 17:59:33 +0200

    highly improved chooseCard, chooseColor added, colored output

--- src/uno/player/console_player.cc
+++ src/uno/player/console_player.cc
@@ -1,14 +1,11 @@
 #include "console_player.h"
+#include "../game/game.h"
 #include <iostream>
-#include "../action/card.h"
 #include <limits> //for istream ignore
+#include <cstring>
+#include <string>
 
 namespace Casino { namespace Uno { namespace Player {
-
-/*using ::Casino::Uno::Action::UnoCard;
-using ::Casino::Uno::Action::CARD_COLOR;
-using ::Casino::Uno::Action::CARD_VALUE;*/
-using namespace ::Casino::Uno::Action;
 
 ConsoleUnoPlayer::ConsoleUnoPlayer(
 	std::istream& in,
@@ -27,18 +24,23 @@ std::ostream& operator<<(std::ostream& out, UnoCard *card) {
 	// print color
 	switch (card->getColor()) {
 	case CARD_COLOR_RED:
-		out << 'R';
+		//out << 'R';
+		out << "\E[37;41m\033[1m";
 		break;
 	case CARD_COLOR_GREEN:
-		out << 'G';
+		//out << 'G';
+		out << "\E[37;42m\033[1m";
 		break;
 	case CARD_COLOR_BLUE:
-		out << 'B';
+		//out << 'B';
+		out << "\E[37;44m\033[1m";
 		break;
 	case CARD_COLOR_YELLOW:
-		out << 'Y';
+		//out << 'Y';
+		out << "\E[38;5;11m\033[1m";
 		break;
 	case CARD_COLOR_BLACK:
+		out << "\E[37;40m\033[1m";
 		break;
 	default:
 		out << '?';
@@ -62,10 +64,10 @@ std::ostream& operator<<(std::ostream& out, UnoCard *card) {
 		CASE_CARD_VALUE(7,"7");
 		CASE_CARD_VALUE(8,"8");
 		CASE_CARD_VALUE(9,"9");
-		CASE_CARD_VALUE(BLOCK, "-block");
-		CASE_CARD_VALUE(REVERSE, "-reverse");
+		CASE_CARD_VALUE(BLOCK, "block");
+		CASE_CARD_VALUE(REVERSE, "reverse");
 		CASE_CARD_VALUE(PLUSTWO, "+2");
-		CASE_CARD_VALUE(COLORPICK, "-colorpicker");
+		CASE_CARD_VALUE(COLORPICK, "colorpicker");
 		CASE_CARD_VALUE(PLUSFOUR, "+4");
 
 		default:
@@ -74,6 +76,8 @@ std::ostream& operator<<(std::ostream& out, UnoCard *card) {
 	}
 
 #undef CASE_CARD_VALUE
+
+	out << "\033[0m\E[0m";
 
 	return out;
 }
@@ -92,48 +96,114 @@ void ConsoleUnoPlayer::listCards() {
 }
 
 int ConsoleUnoPlayer::chooseCard() {
-	out << "-Choose a card: ";
-
 	int choose;
 	bool valid = false;
 
 	while (!valid) {
 		in >> choose;
 
-		valid = true;
-
 		if (!in) {
-			valid = false;
 			in.clear();
-			in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			out << "Choose a number between 1 and " << getCardCount()+1 << ":";
+			in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
 		}
 
 		// range check - +1 because of draw
 		if (choose < 1 || choose > getCardCount() + 1) {
 			valid = false;
 			out << "Invalid card number. Choose a valid card:";
+			in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
 		}
 
-		/** @todo is valid move? */
+		valid = true;
 
+		// check uno
+		char uno[4];
+		uno[0] = in.peek();
+		if (uno[0] == ' ') {
+			in.ignore(1, ' ');
+			in.get(uno, 4, '\n');
+
+			uno[3] = '\0';
+			if (strcmp(uno, "uno") == 0) {
+				setUnoFlag(true);
+			}
+		}
+
+		in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
 
 	return choose - 1;	// -1: index shift
+}
+
+CARD_COLOR ConsoleUnoPlayer::chooseColor() {
+	out << "-Choose a color (red/green/blue/yellow): ";
+	char color_first_letter;
+	CARD_COLOR color = CARD_COLOR_BLACK;
+
+
+	while (color == CARD_COLOR_BLACK) {
+		color_first_letter = in.peek();
+
+		switch (color_first_letter) {
+			case 'r':
+				color = CARD_COLOR_RED;
+				break;
+			case 'g':
+				color = CARD_COLOR_GREEN;
+				break;
+			case 'b':
+				color = CARD_COLOR_BLUE;
+				break;
+			case 'y':
+				color = CARD_COLOR_YELLOW;
+				break;
+			default:
+				out << "-Invalid color (red/green/blue/yellow): ";
+				in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				break;
+		}
+	}
+
+	in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	return color;
 }
 
 UnoAction* ConsoleUnoPlayer::pickAction(UnoGame *game) {
 	println("==Your turn");
 	println("-Your cards:");
 	listCards();
-	int card_index = chooseCard();
 
-	if (card_index == getCardCount()) {
-		//it's a draw
-		return NULL;
-	} else {
-		return hand[card_index];
+	out << "-Choose a card: ";
+	int card_index;
+	UnoAction* picked;
+	bool valid = false;
+	while (!valid) {
+		card_index = chooseCard();
+		if (card_index == getCardCount()) {
+			//it's a draw
+			picked = game->getDrawAction();
+		} else {
+			picked = hand[card_index];
+		}
+
+		std::string message = "";
+		valid = game->isValidMove(picked, message);
+		if (!valid) {
+			out << "Invalid move. " << message <<": ";
+		}
 	}
+
+	/** @todo add color picking */
+	if (card_index != getCardCount()
+	&& static_cast<UnoCard*>(picked)->getColor() == CARD_COLOR_BLACK) {
+		CARD_COLOR new_color = chooseColor();
+		static_cast<WildCard*>(picked)->setColor(new_color);
+	}
+
+	return picked;
 }
 
 void ConsoleUnoPlayer::info(INFO_T type) {
--- src/uno/player/console_player.h
+++ src/uno/player/console_player.h
@@ -2,6 +2,8 @@
 #define CSU_CONSOLE_PLAYER_H_
 
 #include "player.h"
+#include "../action/card.h"
+#include "../action/wild_card.h"
 #include <iostream>
 
 namespace Casino { namespace Uno { namespace Game {
@@ -12,6 +14,8 @@ namespace Casino { namespace Uno { namespace Player {
 
 using ::Casino::Uno::Player::UnoPlayer;
 using ::Casino::Uno::Game::UnoGame;
+//UnoCard, WildCard, CARD_COLOR, CARD_VALUE
+using namespace ::Casino::Uno::Action;
 
 class ConsoleUnoPlayer :public UnoPlayer
 {
@@ -21,6 +25,7 @@ protected:
 	virtual void println(const char* str);
 	virtual void listCards();
 	virtual int chooseCard();
+	virtual CARD_COLOR chooseColor();
 public:
 	ConsoleUnoPlayer(std::istream& in, std::ostream& out);
 	virtual UnoAction* pickAction(UnoGame *game);
