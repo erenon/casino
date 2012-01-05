c01f2d75cc73da676edeed67d29fcdb971def8d1
erenon <erenon2@gmail.com>
2012-01-05 12:02:11 +0100

    PenaltyIndicator (shows the number of cards to draw) and jasmineTests added

---
+++ module/server2/client/js/PenaltyIndicator.js
@@ -0,0 +1,38 @@
+/**
+ * options:
+ *  - pubsub: PubSub object
+ *  - $: jQuery
+ *  - target: DOM element to write into
+ */
+
+var
+PenaltyIndicator = function(options) {
+var pubsub = options.pubsub,
+    $ = options.$,
+    target = options.target,
+    
+    currentPenalty = 0
+    ;
+    
+    pubsub.subscribe('card_played', function(data) {
+        var value = data.played_card.value,
+            addition = 0;
+            ;
+            
+        if (value == '+2') {
+            addition = 2;
+        } else if (value == '+4') {
+            addition = 4;
+        }
+        
+        if (addition > 0) {
+            currentPenalty += addition;
+            target.text(currentPenalty);
+        }
+    });
+    
+    pubsub.subscribe('draw_card', function() {
+        currentPenalty = 0;
+        target.text('');
+    });
+};
---
+++ module/server2/test/spec/testPenaltyIndicator.js
@@ -0,0 +1,34 @@
+describe('PenaltyIndicator', function() {
+var target = $('<div/>').first(),
+    pubsub = PubSub(),
+    pi = PenaltyIndicator({
+        pubsub: pubsub,
+        target: target,
+        $: $
+    });
+
+    it("displays 2 on +2", function() {
+        pubsub.publishSync('card_played', {
+            played_card: {
+                value: '+2'
+            }
+        });
+        
+        expect(target.text()).toEqual('2');
+    });
+    
+    it("increments from 2 to 6", function() {
+        pubsub.publishSync('card_played', {
+            played_card: {
+                value: '+4'
+            }
+        });
+        
+        expect(target.text()).toEqual('6');            
+    });
+    
+    it("resets to 0", function() {
+        pubsub.publishSync('draw_card');
+        expect(target.text()).toEqual('');            
+    });
+});
