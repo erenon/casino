8951dd475d1d5adc7a1cd8e25c2bf3384c2f1da5
erenon <erenon2@gmail.com>
2012-01-15 22:46:02 +0100

    removed unused pubsub dependency

--- module/server2/client/js/SocketioGateway.js
+++ module/server2/client/js/SocketioGateway.js
@@ -1,14 +1,12 @@
 /**
  * options:
- *  - pubsub: PubSub object
  *  - config: Socket.IO config
  *  - io: Socket.IO object
  */
 
 var
 SocketioGateway = function(options) {
-var pubsub = options.pubsub,
-    config = options.config || {
+var config = options.config || {
         host: null,
         connect_options: {port: 3000, rememberTransport: true}
     },
