6cc4225a6b3e8cc870ff1edd3f206a19d4fe90ac
erenon <erenon2@gmail.com>
2012-01-16 11:18:15 +0100

    testPlayer: pass cardBuilder spy to palyers

--- module/server2/test/spec/testPlayer.js
+++ module/server2/test/spec/testPlayer.js
@@ -1,48 +1,82 @@
 describe("Player", function() {
-    it("has name", function () {
-        var player = {},
-            pubsubSpy = { 
-                on: jasmine.createSpy() 
-            }
-            ;
+    var pubsubSpy,
+        player = {}
+        ;
+    
+    beforeEach(function() {
+        pubsubSpy = { 
+            on: jasmine.createSpy() 
+        };
         
         Player({
             $: $,
-            pubsub: pubsubSpy
+            pubsub: pubsubSpy,
+            cardBuilder: {
+                get: jasmine.createSpy().andReturn($('<div>')),
+                getBackside: jasmine.createSpy().andReturn($('<div>')),
+                getCardWidth: jasmine.createSpy().andReturn(100),
+                getCardHeight: jasmine.createSpy().andReturn(200),    
+            }
         }, player);
+    });
         
+    it("has name", function () {       
         player.setName("foo");
         expect(player.getName()).toEqual("foo");
     });
 });
 
 describe("OppositePlayer", function () {
-    it("inherits from player (has name)", function () {
-        var pubsubSpy = { 
-                on: jasmine.createSpy() 
-            },
-            player = OppositePlayer({
-                $: $,
-                pubsub: pubsubSpy
-            });
-            ;
+    var pubsubSpy,
+        player
+        ;
+    
+    beforeEach(function() {
+        pubsubSpy = { 
+            on: jasmine.createSpy() 
+        };
         
+        player = OppositePlayer({
+            $: $,
+            pubsub: pubsubSpy,
+            cardBuilder: {
+                get: jasmine.createSpy().andReturn($('<div>')),
+                getBackside: jasmine.createSpy().andReturn($('<div>')),
+                getCardWidth: jasmine.createSpy().andReturn(100),
+                getCardHeight: jasmine.createSpy().andReturn(200),    
+            }
+        });
+    });
+    
+    it("inherits from player (has name)", function () {       
         player.setName("foo");
         expect(player.getName()).toEqual("foo");        
     });
 });
 
 describe("ControlledPlayer", function () {
-    it("inherits from player (has name)", function () {
-        var pubsubSpy = { 
-                on: jasmine.createSpy() 
-            },
-            player = ControlledPlayer({
-                $: $,
-                pubsub: pubsubSpy
-            });
-            ;
+    var pubsubSpy,
+        player
+        ;
+
+    beforeEach(function() {
+        pubsubSpy = { 
+            on: jasmine.createSpy() 
+        };
         
+        player = ControlledPlayer({
+            $: $,
+            pubsub: pubsubSpy,
+            cardBuilder: {
+                get: jasmine.createSpy().andReturn($('<div>')),
+                getBackside: jasmine.createSpy().andReturn($('<div>')),
+                getCardWidth: jasmine.createSpy().andReturn(100),
+                getCardHeight: jasmine.createSpy().andReturn(200),    
+            }
+        });
+    });    
+    
+    it("inherits from player (has name)", function () {
         player.setName("bar");
         expect(player.getName()).toEqual("bar");        
     });
