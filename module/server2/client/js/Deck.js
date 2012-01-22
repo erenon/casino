d49cb8049f28e5939b0819f25b12103f772c5742
erenon <erenon2@gmail.com>
2012-01-22 14:23:41 +0100

    module Deck and inherited Stock and Pile. Highly improved .movable decorator

--- module/server2/client/js/Deck.js
+++ module/server2/client/js/Deck.js
@@ -4,91 +4,160 @@
  *  - target: DOM element to place the cards into
  *  - pubsub: (optional) PubSub object, listens to game speed changes
  *  - events: eventQueue object, synchronizes moving cards
+ * 
+ * deck: 
  */
 var
-Deck = function(options) {
+Deck = function(options, deck) {
 var $ = options.$,
-    deck = options.target,
+    target = options.target,    // unused
     pubsub = options.pubsub || false,
-    events = options.events || {add: function(eventCallback) {
-        eventCallback(function(){});    
-    }},
-    speed = 400,
-    movable = function(domObject) {
+    deck = deck || {}
+    ;
+    
+    deck.movable = function(domObject) {
+        var margin = 'originalMargin'
+            ;
         
         if (!domObject.movable) {
             domObject.movable = true;
             domObject.enableMove = function() {
-                domObject.css('transition', 'all '+speed/1000+'s ease-in');         
+                domObject.css('transition', 'all '+deck.speed/1000+'s ease-in');
+                domObject.css('position', 'absolute');
+                
+                // store margins
+                domObject.data(margin, {
+                    'top': domObject.css('margin-top'),
+                    'right': domObject.css('margin-right'),
+                    'bottom': domObject.css('margin-bottom'),
+                    'left': domObject.css('margin-left'),
+                });
+                // disable margins
+                domObject.css('margin-top', 0);
+                domObject.css('margin-right', 0);
+                domObject.css('margin-bottom', 0);
+                domObject.css('margin-left', 0);     
             };
             
             domObject.disableMove = function() {
                 domObject.css('transition', 'none');
+                domObject.css('position', 'static');
+                
+                // restore margins
+                domObject.css('margin-top', domObject.data(margin).top);
+                domObject.css('margin-right', domObject.data(margin).right);
+                domObject.css('margin-bottom', domObject.data(margin).bottom);
+                domObject.css('margin-left', domObject.data(margin).left);
+                // unset margin store
+                domObject.data(margin, undefined);
             }
         }
         
         return domObject;
-    }
-    ;
+    };
+    
+    deck.speed = 400;
     
     if (pubsub) {
         pubsub.on('changeGameSpeed', function(data) {
-            speed = data.speed;
+            deck.speed = data.speed;
+        });
+    }
+},
+
+Stock = function(options, deck) {
+var target = options.target,
+    events = options.events || {add: function(eventCallback) {
+        eventCallback(function(){});    
+    }},     // unused
+    deck = deck || {}
+    ;
+    
+    Deck(options, deck);
+    
+    deck.pullCard = function(domCard, endCallback) {
+        var finalOffset = domCard.offset(),
+            cardTarget = domCard.parent(),
+            finalTransform = domCard.css('transform');
+        
+        domCard = domCard.detach();
+        domCard.offset(target.offset());
+        domCard.css('transform', 'none');
+            
+        deck.movable(domCard).enableMove();
+        $('body').append(domCard);
+        
+        domCard.offset(finalOffset);
+        domCard.css('transform', finalTransform);
+        
+        setTimeout(function() {
+            cardTarget.append(domCard);
+            
+            domCard.disableMove();
+            
+            domCard.css('top', '');
+            domCard.css('left', '');
+            
+            if (endCallback) {
+                endCallback();
+            }
+        }, deck.speed);                
+    }
+    
+    return deck;
+},
+
+Pile = function(options, deck) {
+var target = options.target,
+    events = options.events || {add: function(eventCallback) {
+        eventCallback(function(){});    
+    }},
+    pubsub = options.pubsub,
+    stock = options.stock,
+    cardBuilder = options.cardBuilder,
+    deck = deck || {}
+    ;
+    
+    Deck(options, deck);
+    
+    deck.pushCard = function(domCard) {
+        var initialOffset = domCard.offset();
+        
+        domCard.detach();
+        domCard.offset(initialOffset);
+        
+        events.add(function(endCallback) {
+            $('body').append(domCard);
+            
+            deck.movable(domCard).enableMove();
+            domCard.offset(target.offset());
+            
+            setTimeout(function() {
+                //TODO remove previous card if any
+                target.append(domCard);
+                
+                domCard.disableMove();
+                
+                domCard.css('top', '');
+                domCard.css('left', '');
+                
+                endCallback();
+            }, deck.speed);
         });
     }
     
-    return {
-        pullCard: function(domCard) {
-            var finalOffset = domCard.offset(),
-                cardTarget = domCard.parent();
-            
-            domCard = domCard.detach();
-            domCard.offset(deck.offset());
-                
-            events.add(function(endCallback) {
-                $('body').append(domCard);
-                
-                movable(domCard).enableMove();
-                
-                domCard.offset(finalOffset);
-                
-                setTimeout(function() {
-                    cardTarget.append(domCard);
-                    
-                    domCard.disableMove();
-                    
-                    domCard.css('top', '');
-                    domCard.css('left', '');
-                    
-                    endCallback();
-                }, speed);                
-            });
-        },
+    // show first card
+    pubsub.on('game_start', function(data) {
+        var card = data.first_card,
+            domCard = cardBuilder.get(card)
+            ;
         
-        pushCard: function(domCard) {
-            var initialOffset = domCard.offset();
-            
-            domCard.detach();
-            domCard.offset(initialOffset);
-            
-            events.add(function(endCallback) {
-                $('body').append(domCard);
-                
-                movable(domCard).enableMove();
-                domCard.offset(deck.offset());
-                
-                setTimeout(function() {
-                    //TODO remove previous card if any
-                    deck.append(domCard);
-                    
-                    domCard.disableMove();
-                    
-                    domCard.css('top', '');
-                    domCard.css('left', '');
-                    
-                    endCallback();
-                }, speed);
-            });
-        }
-    };
-};
+        events.add(function(endCallback) {
+            target.append(domCard);
+            stock.pullCard(domCard, endCallback);
+        });
+    });
+        
+    return deck;
+}
+;
