27c5081e8d7179efd617f207b93926276f63691f
erenon <erenon2@gmail.com>
2012-01-23 22:13:50 +0100

    Control: initial commit of 'button' module

---
+++ module/server2/client/js/Control.js
@@ -0,0 +1,44 @@
+
+var
+Control = function(options, control) {
+    
+},
+
+DrawButton = function(options, control) {
+var validator = options.validator,
+    socket = options.socket,
+    target = options.target,
+    pubsub = options.pubsub
+    ;
+    
+    target.click(function() {
+        var msg = validator.isDrawValid();
+        if (msg === true) {
+            socket.emit('draw_card');
+        } else {
+            pubsub.emit('invalid_move', {message: msg});
+        }
+    });
+    
+    control = control || {};
+    Control(options, control);
+},
+
+UnoButton = function(options, control) {
+var validator = options.validator,
+    socket = options.socket,
+    target = options.target
+    ;
+    
+    target.click(function() {
+        socket.emit('say_uno');
+        target.addClass('buttonPressed');
+        setTimeout(function() {
+            target.removeClass('buttonPressed');            
+        }, 3000);        
+    });    
+
+    control = control || {};
+    Control(options, control);    
+}
+;
