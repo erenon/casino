ee0494b83bffa28027c72441b8150a13df01485d
erenon <erenon2@gmail.com>
2012-01-24 18:26:28 +0100

    Instantiates Player objects by player_joined event

---
+++ module/server2/client/js/SeatManager.js
@@ -0,0 +1,60 @@
+var
+SeatManager = function(options) {
+var $ = options.$,
+    pubsub = options.pubsub,
+    OppositePlayer = options.OppositePlayer,
+    seats = options.seats,
+    defaultPlayerOptions = options.defaultPlayerOptions,
+    currentSeatIndex = 0,
+    getSeat = function() {
+        var seat;
+        
+        if (currentSeatIndex < seats.length) {
+            // I was tempted here to do this in one statement
+            // by post incrementing currentSeatIndex while indexing the array
+            // but I'm strong in the faith.
+            seat = seats[currentSeatIndex];
+            currentSeatIndex++;
+            return seat;    
+        } else {
+            return undefined;
+        }
+    },
+    addPlayer = function(playerOptions) {
+        // merge default, seat, player options.
+        var options = {}
+            ;
+            
+        $.extend(options, 
+                 defaultPlayerOptions, 
+                 getSeat(), 
+                 playerOptions
+        );
+        
+        return OppositePlayer(options);
+    }
+    ;
+    
+    pubsub.on('player_joined', function(event) {
+        addPlayer({
+            name: event.player.name
+        });
+    });
+    
+    /*return {
+        addPlayer: function(playerOptions) {
+            // merge default, seat, player options.
+            var options = {}
+                ;
+                
+            $.extend(options, 
+                     defaultPlayerOptions, 
+                     getSeat(), 
+                     playerOptions
+            );
+            
+            return OppositePlayer(options);
+        }
+    };*/
+}
+;
