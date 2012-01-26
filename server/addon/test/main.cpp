cc74bd8226d3ea81305f4ffc61e6c5b8101b22da
erenon <erenon2@gmail.com>
2012-01-26 14:18:34 +0100

    Test configuratino main added, inits gtest and gmock

---
+++ server/addon/test/main.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+GTEST_API_ int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	// The following line must be executed to initialize Google Mock
+	// (and Google Test) before running the tests.
+	::testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS();
+}
