0e91e407ef6aa75a76e4346f6f6a628ddd63bd8f
erenon <erenon2@gmail.com>
2012-01-30 09:49:14 +0100

    cpplint updated

--- server/addon/tools/cpplint.sh
+++ server/addon/tools/cpplint.sh
@@ -2,6 +2,6 @@
 TOOL_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 $TOOL_DIR/cpplint.py \
---filter=-legal/copyright,-whitespace/line_length,-whitespace/blank_line,-whitespace/parens \
+--filter=-legal/copyright,-whitespace/line_length,-whitespace/blank_line,-whitespace/parens,-build/header_guard \
 `find $TOOL_DIR/../src/ -not -type d` 2> ./cpplint_report
 
