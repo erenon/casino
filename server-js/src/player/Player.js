6353f5b6d964e139fbcd2750d6099aa5f68554ba
erenon <erenon2@gmail.com>
2012-02-04 12:32:14 +0100

    Player: getCardCount, block, uno public methods added

--- server-js/src/player/Player.js
+++ server-js/src/player/Player.js
@@ -1,8 +1,9 @@
 var
 EventReceiver = require('./EventReceiver');
 Player = function(options, player) {
-/*var hand = []
-    ;*/
+var blockFlag = false,
+    unoFlag = false
+    ;
         
     player = player || {};
     EventReceiver(player);
@@ -15,18 +16,54 @@ Player = function(options, player) {
         console.log(data); 
     });
     
+    player.on('gameEnded', function(data) {
+        console.log(data.winner.name); 
+    });    
+    
     player.on('cardAdded', function(data) {
-        console.log(player.name, data);
+        //console.log(player.name, data);
         player.hand.push(data.card); 
     });
     
+    player.on('cardPlayer', function(data) {
+        //console.log(data.player.name + ' played ' + data.card);
+    });
+    
     player.on('playerGotCard', function(data) {
-        //console.log(data); 
+        //console.log(data.player.name + ' got ' + data.cardCount + ' card(s)'); 
+    });
+    
+    player.on('playerTurn', function(data) {
+       //console.log(player.name + ': turn of ' + data.player.name + ' (cc: ' +data.player.getCardCount()+ ')'); 
     });
     
     player.game = options.game;
     player.name = options.name;
     player.hand = [];
+    
+    player.getCardCount = function() {
+        return player.hand.length;
+    };
+    
+    player.block = function() {
+        if (arguments[0] !== undefined) {
+            blockFlag = arguments[0];
+        }
+        
+        return blockFlag;
+    };
+    
+    player.uno = function() {
+        if (arguments[0] !== undefined) {
+            unoFlag = arguments[0];
+            if (arguments[0] === true) {
+                player.game.sayUno(player);
+            }
+        }
+        
+        return unoFlag;        
+    };
+
     
     return player;
 }
