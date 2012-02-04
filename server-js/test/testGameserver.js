6b8d5e19424308671741b1566c0afce01f0b04a3
erenon <erenon2@gmail.com>
2012-02-04 14:08:41 +0100

    test gameserver added - on socket connect creates quick game

---
+++ server-js/test/testGameserver.js
@@ -0,0 +1,53 @@
+var port = 3001,
+    io = require('socket.io').listen(port),
+    
+    nameManager = require('../src/player/NameManager')(),
+    Game = require('../src/game/Game'),
+    RobotEasyPlayer = require('../src/player/RobotEasyPlayer'),
+    SocketPlayer = require('../src/player/SocketPlayer')
+    ;
+
+
+/*
+ * socket.io setup
+ */
+
+//io.set('log level', 2);
+
+console.log('Socket.IO testGameserver listens on port ' + port);
+
+io.sockets.on('connection', function(socket) {
+    console.log('Client connected to testGameServer');
+    
+    var game = Game({
+            config: {
+                initialHandSize: 7,
+                unoPenalty: 2
+            }
+        }),
+        botA = RobotEasyPlayer({
+            game: game,
+            name: nameManager.getAnonName()
+        }),
+        botB = RobotEasyPlayer({
+            game: game,
+            name: nameManager.getAnonName()
+        }),
+        botC = RobotEasyPlayer({
+            game: game,
+            name: nameManager.getAnonName()
+        }),
+        player = SocketPlayer({
+            game: game,
+            socket: socket,
+            name: 'player'    
+        })
+        ;
+        
+    game.addPlayer(botA);
+    game.addPlayer(botB);
+    game.addPlayer(botC);
+    game.addPlayer(player);   
+    
+    game.start();     
+});
