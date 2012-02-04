5a365180cd61a9abe5a96532c5ade4d08c5da5ff
erenon <erenon2@gmail.com>
2012-02-04 12:31:34 +0100

    PlayerCollection: reverse turn supported, minor jslint changes

--- server-js/src/game/PlayerCollection.js
+++ server-js/src/game/PlayerCollection.js
@@ -2,7 +2,18 @@ var
 PlayerCollection = function() {
 var playerCollection = {},
     players = [],
-    currentIndex 
+    currentIndex,
+    nextPlayerOffset = 1,
+    getNextPlayerIndex = function() {
+        var nextPlayerIndex = currentIndex + nextPlayerOffset,
+            mod = players.length
+            ;
+        
+        // % is the remainder operator in javascript, not the modulo
+        // we have to do this little trick to handle the
+        // 0 -> -1 transition.
+        return (((nextPlayerIndex % mod) + mod) % mod);
+    }
     ;
     
     playerCollection.addPlayer = function(player) {
@@ -17,14 +28,22 @@ var playerCollection = {},
         return players[currentIndex];
     };
     
+    playerCollection.getNextPlayer = function() {
+        return players[getNextPlayerIndex()];    
+    };
+    
     playerCollection.next = function() {
-        currentIndex = (currentIndex + 1) % players.length;
+        currentIndex = getNextPlayerIndex();
         return players[currentIndex];
+    };
+    
+    playerCollection.reverseTurn = function() {
+        nextPlayerOffset *= (-1);
     };
     
     playerCollection.notifyAll = function(event, data) {
         var i;
-        for (i = players.length - 1; i >= 0; i--) {
+        for (i = 0; i < players.length; i++) {
             players[i].notify(event, data);
         }
     };
@@ -32,7 +51,7 @@ var playerCollection = {},
     playerCollection.notifyOthers = function(event, data, player) {
         var i;
         for (i = players.length - 1; i >= 0; i--) {
-            if (players[i] != player) {
+            if (players[i] !== player) {
                 players[i].notify(event, data);
             }
         }        
@@ -40,7 +59,7 @@ var playerCollection = {},
     
     playerCollection.length = function() {
         return players.length;
-    }
+    };
     
     
     playerCollection.reset();
